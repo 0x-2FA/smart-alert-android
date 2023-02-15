7ad213e8ff74342ea90a1a8ab6caf14767ce123e
0x-2FA <2fa_gh@protonmail.com>
2023-02-15 16:37:50 +0200

    Refactored the login in the onCreate method

--- app/src/main/java/com/unipi/projects/smartalert/LoginActivity.java
+++ app/src/main/java/com/unipi/projects/smartalert/LoginActivity.java
@@ -2,20 +2,24 @@ package com.unipi.projects.smartalert;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-import com.unipi.projects.smartalert.Interfaces.ITestApi;
-import com.unipi.projects.smartalert.Model.LoginRequest;
+import com.unipi.projects.smartalert.Model.Auth.AuthResponse;
+import com.unipi.projects.smartalert.Services.Auth.AuthResult;
+import com.unipi.projects.smartalert.Services.Auth.AuthService;
 
-import okhttp3.OkHttpClient;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Single;
+import io.reactivex.rxjava3.core.SingleObserver;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import retrofit2.HttpException;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -24,48 +28,78 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        EditText emailEditText = findViewById(R.id.emailEditText);
+        EditText passwordEditText = findViewById(R.id.passwordEditText);
+
         Button loginButton = findViewById(R.id.loginButton);
 
         loginButton.setOnClickListener(
                 (view) -> {
-                    OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
-                    Retrofit retrofit = new Retrofit.Builder()
-                            .baseUrl("http://192.168.1.75:12021")
-                            .addConverterFactory(GsonConverterFactory.create())
-                            .client(httpClient.build())
-                            .build();
+                    String email = emailEditText.getText().toString().trim();
+                    String password = passwordEditText.getText().toString().trim();
 
-                    ITestApi service = retrofit.create(ITestApi.class);
-                    LoginRequest request = new LoginRequest();
+                    if(email.isEmpty()) {
+                        emailEditText.setError("Email field is required");
+                        return;
+                    }
 
-                    request.setEmail("talepis@unipi.gr");
-                    request.setPassword("12345Ta!");
+                    if(password.isEmpty()) {
+                        passwordEditText.setError("Password field is required");
+                        return;
+                    }
 
-                    Call<LoginRequest> call  = service.Login(request);
+                    AuthService authService = new AuthService();
 
-                    Log.i("call ", call .toString());
+                    Single<AuthResult> singleAuthResult = authService.Login(email, password);
 
-                    call.enqueue(new Callback<LoginRequest>() {
-                        @Override
-                        public void onResponse(
-                                Call<LoginRequest> call,
-                                Response<LoginRequest> response) {
-                            Log.i("res", "Sent successfully!");
-                            Log.i("res", response.body().getEmail());
-                        }
+                    singleAuthResult
+                            .subscribeOn(Schedulers.io())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribe(
+                            new SingleObserver<AuthResult>() {
+                                @Override
+                                public void onSubscribe(@NonNull Disposable d) {
 
-                        @Override
-                        public void onFailure(
-                                Call<LoginRequest> call,
-                                Throwable t) {
-                            Log.i("res", "Something went wrong!");
-                            Log.i("res", t.getMessage());
+                                }
 
-                        }
-                    });
+                                @Override
+                                public void onSuccess(@NonNull AuthResult authResult) {
+                                    authResult.authResponse = new AuthResponse();
+                                    authResult.authResponse.setEmail(email);
 
-                    Toast.makeText(getApplicationContext(),
-                            "Clicked", Toast.LENGTH_SHORT).show();
+                                    Log.i("WOKRS", authResult.authResponse.getEmail());
+
+                                    Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+
+                                    mainIntent.putExtra("email", authResult.authResponse.getEmail());
+
+                                    startActivity(mainIntent);
+                                }
+
+                                @Override
+                                public void onError(@NonNull Throwable e) {
+                                    Log.e("WOKRS", e.getMessage());
+                                    if(e instanceof HttpException) {
+                                        HttpException error = (HttpException) e;
+                                        String errorResponse = error.response().message();
+
+
+                                        if(errorResponse.contains("Bad")) {
+
+                                            Toast.makeText(getApplicationContext(),
+                                                    "User not found", Toast.LENGTH_SHORT).show();
+                                            return;
+                                        }
+                                        Log.e("WORKS", errorResponse);
+
+                                    }
+
+                                    Toast.makeText(getApplicationContext(),
+                                            "Failed to connect to server", Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                    );
                 }
         );
     }
