e0e158ff4bc5f27332e5ca1c4d6bbbd6f14151ef
0x-2FA <2fa_gh@protonmail.com>
2023-02-18 23:03:55 +0200

    Implemented the Register functionality on button click

--- app/src/main/java/com/unipi/projects/smartalert/RegisterActivity.java
+++ app/src/main/java/com/unipi/projects/smartalert/RegisterActivity.java
@@ -2,7 +2,23 @@ package com.unipi.projects.smartalert;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.unipi.projects.smartalert.Services.Auth.AuthResult;
+import com.unipi.projects.smartalert.Services.Auth.AuthService;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Single;
+import io.reactivex.rxjava3.core.SingleObserver;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import retrofit2.HttpException;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +26,82 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        EditText emailEditText = findViewById(R.id.emailEditText);
+        EditText phoneEditText = findViewById(R.id.phoneEditText);
+        EditText passwordEditText = findViewById(R.id.passwordEditText);
+
+        Button registerButton = findViewById(R.id.registerButton);
+
+        registerButton.setOnClickListener(
+                (view) -> {
+                    String email = emailEditText.getText().toString().trim();
+                    String phone = phoneEditText.getText().toString().trim();
+                    String password = passwordEditText.getText().toString().trim();
+
+                    if(email.isEmpty()) {
+                        emailEditText.setError("Email field is required");
+                        return;
+                    }
+
+                    if(phone.isEmpty()) {
+                        emailEditText.setError("Phone field is required");
+                        return;
+                    }
+
+                    if(password.isEmpty()) {
+                        passwordEditText.setError("Password field is required");
+                        return;
+                    }
+
+                    AuthService authService = new AuthService();
+
+                    Single<AuthResult> singleAuthResult = authService.Register(email, phone, password);
+
+                    singleAuthResult
+                            .subscribeOn(Schedulers.io())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribe(
+                                    new SingleObserver<AuthResult>() {
+                                        @Override
+                                        public void onSubscribe(@NonNull Disposable d) {
+
+                                        }
+
+                                        @Override
+                                        public void onSuccess(@NonNull AuthResult authResult) {
+
+                                            Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
+
+                                            mainIntent.putExtra("email", authResult.getEmail());
+                                            mainIntent.putExtra("userId", authResult.getUserId());
+
+                                            startActivity(mainIntent);
+                                        }
+
+                                        @Override
+                                        public void onError(@NonNull Throwable e) {
+                                            Log.e("WOKRS", e.getMessage());
+                                            if(e instanceof HttpException) {
+                                                HttpException error = (HttpException) e;
+                                                String errorResponse = error.response().message();
+
+                                                if(errorResponse.contains("Bad")) {
+
+                                                    Toast.makeText(getApplicationContext(),
+                                                            "Invalid credentials", Toast.LENGTH_SHORT).show();
+                                                    return;
+                                                }
+                                                Log.e("WORKS", errorResponse);
+
+                                            }
+
+                                            Toast.makeText(getApplicationContext(),
+                                                    "Failed to connect to server", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                            );
+                }
+        );
     }
 }
