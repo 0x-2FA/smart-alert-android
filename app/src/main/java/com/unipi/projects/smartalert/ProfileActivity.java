71c3d31c66d52a8241a0e4379ad9794af3a80326
0x-2FA <2fa_gh@protonmail.com>
2023-02-20 00:29:26 +0200

    Added some logic to the ProfileActivity

--- app/src/main/java/com/unipi/projects/smartalert/ProfileActivity.java
+++ app/src/main/java/com/unipi/projects/smartalert/ProfileActivity.java
@@ -2,7 +2,18 @@ package com.unipi.projects.smartalert;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import java.util.Locale;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,5 +21,67 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        setupSpinnerLangSelection();
+        onSpinnerChanged();
+
+        Button newEventButton = findViewById(R.id.newEventButton);
+
+        newEventButton.setOnClickListener(
+                (view) -> {
+                    Intent mainIntent = new Intent(ProfileActivity.this, MainActivity.class);
+                    startActivity(mainIntent);
+                }
+        );
+    }
+
+    private void setupSpinnerLangSelection() {
+        Spinner spinner = findViewById(R.id.selectLangSpinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.lang_array, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+    }
+
+    private void onSpinnerChanged() {
+        Spinner spinner = findViewById(R.id.selectLangSpinner);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                if (adapterView.getSelectedItemPosition() == 1)
+                {
+                    changeLocale("en");
+                }
+                else if(adapterView.getSelectedItemPosition() == 2)
+                {
+                    changeLocale("el");
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+    private void changeLocale(String language) {
+        Locale locale = new Locale(language);
+        Resources resources = this.getResources();
+        DisplayMetrics displayMetrics = resources.getDisplayMetrics();
+
+        Locale.setDefault(locale);
+
+        Configuration config = resources.getConfiguration();
+        config.setLocale(locale);
+
+        resources.updateConfiguration(config, displayMetrics);
     }
 }
