316bffa642f55728b1f8690141976296b573d844
0x-2FA <2fa_gh@protonmail.com>
2023-05-01 17:43:06 +0300

    Added actual lat/long data and support for changing language

--- app/src/main/java/com/unipi/projects/smartalert/MainActivity.java
+++ app/src/main/java/com/unipi/projects/smartalert/MainActivity.java
@@ -1,22 +1,30 @@
 package com.unipi.projects.smartalert;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.unipi.projects.smartalert.Services.Auth.AuthResult;
-import com.unipi.projects.smartalert.Services.Auth.AuthService;
 import com.unipi.projects.smartalert.Services.Events.EventResult;
 import com.unipi.projects.smartalert.Services.Events.EventService;
 
@@ -24,17 +32,26 @@ import java.util.Locale;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.annotations.NonNull;
-import io.reactivex.rxjava3.core.Observer;
 import io.reactivex.rxjava3.core.Single;
 import io.reactivex.rxjava3.core.SingleObserver;
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LocationManager _locationManager;
+    private LocationListener _locationListener;
+
+    private Double _lat;
+    private Double _long;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupSpinnerLangSelection();
+        onSpinnerChanged();
 
         // getting data from intent
         String email = getIntent().getStringExtra("email");
@@ -42,27 +59,64 @@ public class MainActivity extends AppCompatActivity {
 
         setupSpinnerSelection();
 
+        locationRequest();
+
+        _locationListener = setLocationListener();
+        _locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
         EditText editTextMultiLine = findViewById(R.id.editTextMultiLine);
 
         Button sendEventButton = findViewById(R.id.sendEventButton);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            _locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0,
+                    0, _locationListener);
+
+            Log.i("MOT", "ENVOKED locationManager");
+        }
 
         sendEventButton.setOnClickListener(
                 (view) -> {
                     Spinner spinner = findViewById(R.id.selectTypeSpinner);
                     String selectedType = spinner.getSelectedItem().toString();
 
+                    Single<EventResult> singleEventResult;
                     EventService eventService = new EventService();
 
                     if(editTextMultiLine.getText().toString().isEmpty()) {
-                        Single<EventResult> singleEventResult = eventService
+                        singleEventResult = eventService
                                 .SendEvent(
                                     selectedType,
-                                    "132",
-                                    "123",
+                                        "123",
+                                        "123",
+                                    //_lat.toString(),
+                                    //_long.toString(),
+                                    userId
+                                );
+
+                    }
+                    else {
+
+                        String comment = editTextMultiLine.getText().toString();
+
+                        singleEventResult = eventService
+                                .SendEvent(
+                                        selectedType,
+                                        "123",
+                                        "123",
+//                                        _lat.toString(),
+//                                        _long.toString(),
+                                        comment,
                                         userId
                                 );
-                        singleEventResultSubscription(singleEventResult);
                     }
+
+                    singleEventResultSubscription(singleEventResult);
+
+                    String toastText = this.getResources().getString(R.string.location_sent_successfully);
+
+                    Toast.makeText(getApplicationContext(),
+                            toastText, Toast.LENGTH_SHORT).show();
                 }
         );
     }
@@ -76,6 +130,72 @@ public class MainActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         spinner.setAdapter(adapter);
+    }
+
+    private void locationRequest() {
+        ActivityResultLauncher<String> requestPermissionLauncher =
+                registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                    Button sendEventButton = findViewById(R.id.sendEventButton);
+
+                    if (isGranted) {
+                        sendEventButton.setClickable(true);
+                        sendEventButton.setLongClickable(true);
+
+                    } else {
+                        sendEventButton.setClickable(false);
+                        sendEventButton.setLongClickable(false);
+
+                        String title = this.getResources().getString(R.string.declined_location_request_title);
+                        String message = this.getResources().getString(R.string.declined_location_request_message);
+
+                        showMessage(title, message);
+                    }
+                });
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+    }
+
+    private void showMessage(String title, String message) {
+        new AlertDialog.Builder(this)
+                .setCancelable(true)
+                .setTitle(title)
+                .setMessage(message)
+                .show();
+    }
+
+    private LocationListener setLocationListener() {
+        return  new LocationListener() {
+            @Override
+            public void onLocationChanged(@androidx.annotation.NonNull Location location) {
+                _lat = location.getLatitude();
+                _long = location.getLongitude();
+                Log.i("MOT", "ENVOKED onLocationChanged");
+
+                Log.i("MOT", "_lat = " + _lat.toString());
+                Log.i("MOT", "location = " + location);
+
+            }
+
+            @Override
+            public void onFlushComplete(int requestCode) {
+                //LocationListener.super.onFlushComplete(requestCode);
+            }
+
+            @Override
+            public void onProviderEnabled(@androidx.annotation.NonNull String provider) {
+                //LocationListener.super.onProviderEnabled(provider);
+            }
+
+            @Override
+            public void onProviderDisabled(@androidx.annotation.NonNull String provider) {
+                //LocationListener.super.onProviderDisabled(provider);
+                Log.e("MOT", provider);
+            }
+        };
     }
 
     private void singleEventResultSubscription(@NonNull Single<EventResult> singleEventResult) {
@@ -103,5 +223,55 @@ public class MainActivity extends AppCompatActivity {
                             }
                         }
                 );
+    }
+
+    private void setupSpinnerLangSelection() {
+        Spinner spinner = findViewById(R.id.selectLangSpinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.lang_array, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+    }
+
+    private void onSpinnerChanged() {
+        Spinner spinner = findViewById(R.id.selectLangSpinner);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                if (adapterView.getSelectedItemPosition() == 1)
+                {
+                    changeLocale("en");
+                }
+                else if(adapterView.getSelectedItemPosition() == 2)
+                {
+                    changeLocale("el");
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+    private void changeLocale(String language) {
+        Locale locale = new Locale(language);
+        Resources resources = this.getResources();
+        DisplayMetrics displayMetrics = resources.getDisplayMetrics();
+
+        Locale.setDefault(locale);
+
+        Configuration config = resources.getConfiguration();
+        config.setLocale(locale);
+
+        resources.updateConfiguration(config, displayMetrics);
     }
 }
